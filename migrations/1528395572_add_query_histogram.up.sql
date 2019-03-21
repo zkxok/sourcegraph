b86792942f921b1cca95783665ad68beeb557800
Issac Trotts <ijt@sourcegraph.com>
2019-03-21 16:39:20 -0700

    flesh out the sql files

--- migrations/1528395572_add_query_histogram.up.sql
+++ migrations/1528395572_add_query_histogram.up.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
--- Insert migration here. See README.md. Highlights:
---  * Always use IF EXISTS. eg: DROP TABLE IF EXISTS global_dep_private;
---  * All migrations must be backward-compatible. Old versions of Sourcegraph
---    need to be able to read/write post migration.
---  * Historically we advised against transactions since we thought the
---    migrate library handled it. However, it does not! /facepalm
+CREATE TABLE "query_histogram" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"query" text NOT NULL,
+	-- how many times query has been run since this table was created
+	"count" integer NOT NULL
+);
 
 COMMIT;
