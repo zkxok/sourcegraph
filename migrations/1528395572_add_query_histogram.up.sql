9edec5bbc179b574dd113a281c82217ec8f99e3b
Issac Trotts <ijt@sourcegraph.com>
2019-03-21 17:08:56 -0700

    add unique constraint to query column

--- migrations/1528395572_add_query_histogram.up.sql
+++ migrations/1528395572_add_query_histogram.up.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 CREATE TABLE "query_histogram" (
 	"id" serial NOT NULL PRIMARY KEY,
-	"query" text NOT NULL,
+	"query" text NOT NULL UNIQUE,
 	-- how many times query has been run since this table was created
 	"count" integer NOT NULL
 );
