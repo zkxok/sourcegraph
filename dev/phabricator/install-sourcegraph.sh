2f30fbe53c640be373e25d5f9698bb7e596ad4db
Loïc Guychard <loic@sourcegraph.com>
2019-05-14 13:49:15 +0200

    Add script to install the Sourcegraph integration

---
+++ dev/phabricator/install-sourcegraph.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sourcegraph_extension="https://github.com/sourcegraph/phabricator-extension.git"
+
+phab_container=$(docker ps -aq -f name=phabricator$)
+phab_directory="/opt/bitnami/phabricator"
+
+# Install the Phabricator native extension
+docker exec -it $phab_container \
+    sh -c "cd ${phab_directory}/src/extensions && git clone -b release-v1.1 ${sourcegraph_extension} sourcegraph"
+
+# Add the static CSS/JS assets
+docker exec -it $phab_container \
+    sh -c "cd ${phab_directory} && ./bin/celerity map"
+
+# Restart phabricator
+source ./restart.sh
+
+sourcegraph_url="${SOURCEGRAPH_URL}"
+if [ -z $SOURCEGRAPH_URL ]
+then
+    sourcegraph_url="http://127.0.0.1:3080"
+fi
+
+# Set sourcegraph.url config value
+docker exec -it $phab_container \
+    sh -c "cd ${phab_directory} && ./bin/config set sourcegraph.url ${sourcegraph_url} --database"
