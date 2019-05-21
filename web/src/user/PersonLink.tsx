f838d770ba83e4d5a768bdc2b37df17f084ae2da
Quinn Slack <qslack@qslack.com>
2019-05-20 19:08:21 -0700

    support PersonLink elements without a separate displayName
    This makes it possible to render a `<PersonLink />` when all you have is a `GQL.IUser`.

--- web/src/user/PersonLink.tsx
+++ web/src/user/PersonLink.tsx
@@ -6,25 +6,26 @@ import * as GQL from '../../../shared/src/graphql/schema'
  * A person's name, with a link to their Sourcegraph user profile if an associated user is found.
  */
 export const PersonLink: React.FunctionComponent<{
-    displayName: string
+    displayName?: string
     user?: GQL.IUser | null
     className?: string
     userClassName?: string
 }> = ({ displayName, user, className = '', userClassName }) => (
     <>
-        <span className={className}>{displayName}</span>
-        {user && (
+        {displayName && (
             <>
-                {' '}
-                &mdash;{' '}
-                <Link
-                    to={user.url}
-                    className={`${className} ${userClassName || ''}`}
-                    title={user.displayName || displayName}
-                >
-                    {user.username}
-                </Link>
+                <span className={className}>{displayName}</span>
+                {user && <> &mdash; </>}
             </>
+        )}
+        {user && (
+            <Link
+                to={user.url}
+                className={`${className} ${userClassName || ''}`}
+                title={user.displayName || displayName}
+            >
+                {user.username}
+            </Link>
         )}
     </>
 )
